a82a6771d078e9bc96be380723a02537a4b052e3
unknown <maks17990@mail.ru>
2019-08-14 20:24:57 +0300

    22

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,25 @@
-const path = require("path");
-const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-let NODE_ENV = 'production'
-if (process.env.NODE_ENV) {
-    NODE_ENV = process.env.NODE_ENV.replace(/^\s+|\s+$/g, "")
-}
 
-var config = {
-    entry: `${__dirname}/src/index.js`,
-
-    node: {
-        fs: "empty"
+module.exports = {
+    resolve: { 
+        extensions: ['.js',] 
     },
-     
+    entry: {
+        'client_packages': './client_packages',
+    },
     output: {
-         path: __dirname,
-         publicPath: "/",
-         filename: '../client_packages/gamemode/browser/js/build.js',
+        path: path.resolve(__dirname),
+        filename: '[name]/index.js'
     },
-
-    watch: NODE_ENV == 'production',
-     
-    devServer: {
-         inline: true,
-         port: 8080
-    },
-
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-     
-    module: {
-         rules: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['es2015', 'stage-0', 'react']
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.(jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                    use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
-                }
-         ]
-    }
-}
-
-module.exports = config;
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: './client_packages/gamemode/browser/js', to: 'client_packages/RP/Browsers' },
+            { from: './client_packages/gamemode/scripts', to: 'client_packages/_rage-console' }
+        ])
+    ]
+};
