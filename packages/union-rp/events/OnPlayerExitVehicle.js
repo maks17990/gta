d4e4497f22344c4e65eb023283319cb0460f3a80
unknown <maks17990@mail.ru>
2019-08-14 19:29:38 +0300

    1

--- packages/union-rp/events/OnPlayerExitVehicle.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "playerExitVehicle": (player, vehicle) => {
-        if (vehicle.player == player) delete vehicle.player;
-
-        if (player && player.hasCuffs) player.playAnimation("mp_arresting", 'idle', 1, 49);
-        mp.setVehSpawnTimer(vehicle);
-    }
-}
